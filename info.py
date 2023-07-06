0450998a94507525df46e04d645513ec35888098
Natalia <natalia.heine@gmail.com>
2023-07-06 03:22:42 +0200

    Den Link auf GitHub hinzugefügt

--- info.py
+++ info.py
@@ -5,4 +5,4 @@ def start():
 
     st.success("In diesem Projekt wurden die Daten von der Webseite 'AutoScout24' analysiert.  \n\nDas Dataset findet man unter folgendem [Link](https://www.kaggle.com/datasets/ander289386/cars-germany).")
             
-    st.success("Unter diesem [Link](https://www.github.com) kann man das gesamte Projekt auf GitHub anschauen.  \n\nDort findet man unter anderem ein Jupyter Notebook, in dem zu sehen ist, wie das Modell ausgewählt, trainiert und gespeichert wurde.")
+    st.success("Unter diesem [Link](https://github.com/nataliaheine/autoscout24) kann man das gesamte Projekt auf GitHub anschauen.  \n\nDort findet man unter anderem ein Jupyter Notebook, in dem zu sehen ist, wie das Modell ausgewählt, trainiert und gespeichert wurde.")
